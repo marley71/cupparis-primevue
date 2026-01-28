179246a70960873a6732911578a670bdf18b22f8
Giacomo Terreni <giacomo.terreni@gmail.com>
2026-01-28 16:44:52 +0100

    cmanage constrained e wcheckbox string

--- src/widgets/_wCheckbox.vue
+++ src/widgets/_wCheckbox.vue
@@ -6,5 +6,19 @@ import _wBase from "./_wBase.vue"
 export default {
     name: '_wCheckbox',
     extends : _wBase,
+  created() {
+    this.setValue(this.value);
+  },
+  methods: {
+    setValue(value) {
+      //console.debug('_wBase.setValue',value);
+      if (Array.isArray(value)) {
+        for (var i in value) {
+          value[i] = value[i] + '';
+        }
+      }
+      this.value = value;
+    },
+  }
 }
 </script>
