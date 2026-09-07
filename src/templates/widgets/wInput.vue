12b0293d4054b01738b4f7c6801e40293615098f
vagrant <vagrant@homestead>
2026-09-07 09:22:19 -0500

    aggiunta gestione watch in password

--- src/templates/widgets/wInput.vue
+++ src/templates/widgets/wInput.vue
@@ -1,12 +1,20 @@
 <template>
   <span>
-    <Password v-if="inputType == 'password'" :inputProps="{'name':name}" :name="name" v-model="value" toggleMask
-              v-bind="extraBind" fluid
-              class="w-full"
-              @change="change" :class="errors.length?'p-invalid':''"
-              :promptLabel="translate('app.scegli-password')" :weakLabel="translate('app.password-semplice')"
-              :mediumLabel="translate('app.password-media')"
-              :strongLabel="translate('app.password-complessa')"/>
+    <template v-if="inputType == 'password'">
+      <IconField>
+        <InputIcon class="fa fa-lock text-black/70!" />
+        <Password :inputProps="{'name':name}" :name="name" v-model="value" toggleMask
+                v-bind="extraBind" fluid
+                :toggleMask="true"
+                class="w-full"
+                @change="change" :class="errors.length?'p-invalid':''"
+                :promptLabel="translate('app.scegli-password')" :weakLabel="translate('app.password-semplice')"
+                :mediumLabel="translate('app.password-media')"
+                :strongLabel="translate('app.password-complessa')"/>
+       
+      </IconField>
+    </template>
+    
     <InputText v-else class="w-full" :name="name" :type="inputType" v-model="value" v-bind="extraBind"
              @change="change" :class="errors && errors.length?'p-invalid':''"></InputText>
     <RulesErrors :errors="errors"></RulesErrors>
@@ -21,7 +29,12 @@ import RulesErrors from "./RulesErrors.vue";
 export default {
   name: "wInput",
   extends: _wInput,
-  components : {RulesErrors}
+  components : {RulesErrors},
+  data() {
+    return {
+      showPassword: false
+    }
+  }
 }
 
 </script>
